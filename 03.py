e191121defe49849916dbeb6b4735616e614afef
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-05 15:39:45 +0200

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,12 @@
+# 3. Ülesanne
+#Eliise Lisete Kristal
+
+nimi = "Eliise" #sõne, string, str
+vanus = 21 #int, integer, täisarv
+Keskmine_hinne = 5.9 #komarv, float
+
+print(nimi+", " +str(vanus)+" aastat vana ja keskmine hinne on "+str(Keskmine_hinne))
+print(nimi,",",vanus,"aastat vana ja keskmine hinne on",Keskmine_hinne)
+print(f"{nimi}, {vanus} aastat vana ja keskmine hinne on {Keskmine_hinne}")
+print(f"{vanus} aastat vana on {nimi} ja tema keskmine hinne on {Keskmine_hinne}")
+
