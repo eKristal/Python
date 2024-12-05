0235ecb59eb3b767529b6dde179d0c0e522c0f71
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-05 14:50:19 +0200

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,10 +1,10 @@
 # 01. ülesanne
 # Eliise Lisete Kristal ITS-24
 
-
 import turtle
 
-turtle.speed(0)
+#kolmnurk
+turtle.speed(0) #reguleeri 1-9
 turtle.penup()
 turtle.goto(-500,200)
 turtle.pendown()
@@ -15,6 +15,16 @@ turtle.forward(200)#fd,  pikslites
 turtle.left(120)
 turtle.forward(200)#fd,  pikslites
 turtle.left(120)
+
+#süda
+turtle.penup()
+turtle.goto(-200,200)
+turtle.pendown()
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.right (90)
+turtle.circle(50,180)
+turtle.fd(100)
 
 
 #lõpetab kilpkonna
