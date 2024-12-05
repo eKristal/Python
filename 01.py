c7e82ac09d6212bd9481a362f43fec53066f367e
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-05 14:37:29 +0200

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,21 @@
+# 01. ülesanne
+# Eliise Lisete Kristal ITS-24
+
+
+import turtle
+
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-500,200)
+turtle.pendown()
+
+turtle.forward(200)#fd,  pikslites
+turtle.left(120)
+turtle.forward(200)#fd,  pikslites
+turtle.left(120)
+turtle.forward(200)#fd,  pikslites
+turtle.left(120)
+
+
+#lõpetab kilpkonna
+turtle.done()
