e43bc9357f388735c07d199ffea29ff7bad1e4fc
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-05 15:55:59 +0200

    Update 03.py

--- 03.py
+++ 03.py
@@ -10,3 +10,48 @@ print(nimi,",",vanus,"aastat vana ja keskmine hinne on",Keskmine_hinne)
 print(f"{nimi}, {vanus} aastat vana ja keskmine hinne on {Keskmine_hinne}")
 print(f"{vanus} aastat vana on {nimi} ja tema keskmine hinne on {Keskmine_hinne}")
 
+#kolmnurk
+
+import turtle
+
+kylje_pikkus = 100
+nurk = 120
+varv = "pink"
+
+turtle.color(varv)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+
+
+turtle.penup()
+turtle.goto(kylje_pikkus*1.5,0)
+turtle.pendown()
+
+turtle.color(varv)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+
+turtle.penup()
+turtle.goto(kylje_pikkus*3,0)
+turtle.pendown()
+
+turtle.color(varv)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+turtle.forward(kylje_pikkus)
+turtle.left(nurk)
+
+
+
+
+
