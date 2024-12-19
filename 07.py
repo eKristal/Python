ce1482ce40aad24718e2d1606599803fabcd0d01
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-19 17:40:29 +0200

    Update 07.py

--- 07.py
+++ 07.py
@@ -3,8 +3,11 @@
 nimi= ["Jyri", "Mari", "Maali", "Bob"]
 
 # print(nimi[0])
-for i in nimi:
-    print(i)
+# for i in nimi:
+#     print(i)
     
 for i in range(4):
     print(f"{i+1}. {nimi[i]}")
+
+valik= int(input("Vali lugu (1-4): "))-1
+print(f"Mängin: {nimi[valik-1]}")
