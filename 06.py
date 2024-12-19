7441f64fbd71b64eb02bf6f3632f4ad67a3b93ba
eKristal <eliiselisete@hkhk.edu.ee>
2024-12-19 17:23:15 +0200

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,6 +1,7 @@
 # Ülesanne 6
 
 import math
+import turtle
 
 nurk= 53
 korgus= 4.4
@@ -10,4 +11,14 @@ kaugus = korgus / math.tan(rad)
 c = math.sqrt(math.pow(kaugus,2)+math.pow(korgus,2))
 
 print(c)
+kordaja = 10
+turtle.fd(kaugus*kordaja)
+turtle.lt(90)
+turtle.fd(korgus*kordaja)
+turtle.rt(360-53-90)
+turtle.fd(c*kordaja)
+
+
+turtle.done()
+
 
