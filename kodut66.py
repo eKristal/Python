a0096c05c8b5b916da4fbe8c7513c77be05b9a10
eKristal <eliiselisete@hkhk.edu.ee>
2025-01-30 21:11:45 +0200

    update

--- kodut66.py
+++ kodut66.py
@@ -1,51 +1,16 @@
 import turtle
 
-size = 100
+suurus = 100  
 
 for _ in range(6):
-    turtle.forward(size)
+    turtle.forward(suurus)
     turtle.left(60)
-
-for _ in range(1):
-    turtle.forward(size)
-    turtle.left(60)
-    turtle.forward(size)
-    turtle.left(120)
-    turtle.forward(size)
-    turtle.left(120)
-    turtle.forward(size)
-    turtle.left(60)
-    turtle.backward(size)
-
-for _ in range(1):
-    turtle.forward(size)
-    turtle.left(120)
-    turtle.forward(size)
-    turtle.left(60)
-    turtle.forward(size)
-    turtle.left(120)
-    turtle.forward(size)
-    turtle.right(60)
-    turtle.backward(size)
-
-for _ in range(1):
-    turtle.backward(size)
-    turtle.right(120)
-
-for _ in range(1):
-    turtle.left(120)
-    turtle.forward(size)
-    turtle.right(60)
-    turtle.forward(size)
-    turtle.left(60)
-    turtle.backward(size)
-    turtle.left(60)
-    turtle.forward(size)
     
+    for _ in range(3):
+        turtle.forward(suurus)
+        turtle.left(120)
     
-
-
-
+    turtle.backward(suurus)  
 
 
 turtle.done()
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-print ("test")
-print ("Hello World")
+import turtle
+
+suurus = 100  
+
+for _ in range(6):
+    turtle.forward(suurus)
+    turtle.left(60)
+    
+    for _ in range(3):
+        turtle.forward(suurus)
+        turtle.left(120)
+    
+    turtle.backward(suurus)  
+
+
+turtle.done()
+
