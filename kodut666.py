46bcb9b412b849151e3e7d854b299b0ea40e0f69
eKristal <eliiselisete@hkhk.edu.ee>
2025-01-30 21:42:31 +0200

    update

--- kodut666.py
+++ kodut666.py
@@ -3,14 +3,10 @@ import turtle
 suurus = 100  
 
 for _ in range(6):
-    turtle.forward(suurus)
-    turtle.left(60)
-    
     for _ in range(3):
         turtle.forward(suurus)
         turtle.left(120)
     
-    turtle.backward(suurus)
-
+    turtle.left(60)
 
 turtle.done()
