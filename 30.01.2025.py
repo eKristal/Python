99f63cdfaf32c2e566fda669627247b45b550bf4
ekristal <eliiselisete@hkhk.edu.ee>
2025-01-30 13:42:01 +0200

    update

---
+++ 30.01.2025.py
@@ -0,0 +1,39 @@
+#Koostada programm, mis väljastaks ekraanile teksti Tere, maailm! täpselt sellisel kujul - koma ja hüüumärgiga.
+
+# print("Tere, maailm!")
+
+#Ül. "Pilved"
+#Pilvede alumise pinna (aluse) kõrguse järgi liigitatakse pilvi ülemise, keskmise ja alumise kihi pilvedeks. Ülemiste pilvede alus on kõrgemal kui 6 km, keskmistel pilvedel on 2-6 km kõrgusel, alumistel pilvedel on madalamal kui 2 km. Koostada programm, mis
+
+
+# korgus = float(input("Sisesta pilvede kõrgus: "))
+# if korgus > 6:
+#     print("Ülemised")
+# elif korgus > 2 and korgus <= 6:
+#     print("Keskmised")
+# else:
+#     print("Alumised")
+
+#Ül. "Bussid"
+#Meil on vaja transportida teatud arv inimesi mingi arvu identsete bussidega.
+# Eeldame, et reisijaid on vähemalt üks. Koostada programm, mis küsib transporditavate
+# inimeste arvu ja ühe bussi kohtade arvu (just sellises järjekorras) ning väljastab
+# ekraanile, mitu bussi on vaja ja mitu inimest on viimases bussis (eeldusel, et kõik
+# eelnevad bussid on täis).
+
+# import math
+
+# inim= 60
+# koht= 40
+# viimane_inim_arv = abs(inim-koht)
+
+# busside_arv =  math.ceil (inim/koht)
+# if inim==koht:
+#     viimane_inim_arv = inim
+# else:
+#     viimane_inim_arv = abs(inim-koht)
+
+# print(f"Busside arv: {busside_arv}")
+# print(f"Viimases bussis on inimesi: {viimane_inim_arv}")
+
+
