eef0bbb683589f6211e21c4d302435cdc07b2eb7
eKristal <eliiselisete@hkhk.edu.ee>
2025-02-28 14:41:30 +0200

    -

---
+++ else-if-elif.py
@@ -0,0 +1,51 @@
+#Ülesanne 5 (vanusepiirang)
+
+# vanus= int(input("Sisesta oma vanus: "))
+# if vanus >=18:
+#     print("Oled täisealine!")
+# if vanus <18:
+#     print("Oled alaealine!")
+
+#Ülesanne 5.1 (ilmaennustuse rakendus)
+
+# temperatuur= int(input("Sisesta temperatuur: "))
+    
+# if temperatuur >=15:
+#     print("Kanna suveriideid!")
+# elif  0 <= temperatuur <=15:
+#     print("Kanna kevad-sügis rõivaid!")
+# else:
+#     print("Kanna talverõivaid!")
+
+#Ülesanne 5.2 (Matemaatika test)
+
+# import random
+
+# arv1= random.randint(1,10)
+# arv2= random.randint(1,10)
+# oige_vastus= arv1*arv2
+
+# vastus=int(input(f"Palju on {arv1}x{arv2}? "))      #Fstring - lisab arvud 
+# if vastus == oige_vastus:
+#     print("Õige vastus!")
+# else: 
+#     print("Vale vastus, proovi uuesti.")
+
+#Ülesanne 5.3 (Mündiviskamise äraarvamine koos juhuslikkusega)
+
+# import random
+# import turtle
+
+# tulemus= random.randint(0,1)
+# vastus= input("Kas kull või kiri?")
+
+# if tulemus ==0:
+#     õige_vastus= "kiri"
+# else: õige_vastus= "kull"
+
+# if vastus == õige_vastus:
+#     print("Õige vastus!")
+# else:
+#     print("Vale vastus! Õige vastus oleks olnud:", õige_vastus)
+
+
